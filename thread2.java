47876410447dc7b970b73bbdfb433f61eceef5f6
Abhinav shukla <abhinavshuklaa632@gmail.com>
2025-06-04 11:09:07 +0530

    Add files via upload

---
+++ thread2.java
@@ -0,0 +1,28 @@
+class FileDownload extends Thread{
+    String fileName;
+
+    public FileDownload(String fileName){
+        this.fileName=fileName;
+    }
+    public void run(){
+        for (int i=1;i<=5;i++)
+        {
+            System.out.println(fileName+" "+"downloading.."+(i*20)+ "%");
+            try {
+                Thread.sleep(2000);
+            }catch (Exception e){
+        }
+    }
+        System.out.println(fileName+ " done");
+}
+}
+
+public class thread2 {
+    public static void main(String[] args) {
+        FileDownload t1=new FileDownload("File A");
+        FileDownload t2=new FileDownload("File B");
+        t1.start();
+        t2.start();
+    }
+}
+
